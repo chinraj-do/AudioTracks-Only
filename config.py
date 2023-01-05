24a51cc339044cef69944c03a0273b93ca289b91
chinraj <117540121+chinraj-do@users.noreply.github.com>
2023-01-05 11:31:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1075808797"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Tamilaudiotracks:<password>@chinjpko.qut5tjt.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Tamilaudiotracks:Micromaxa@8098@chinjpko.qut5tjt.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "ChinJpko")
 
 #force sub channel id, if you want enable force sub
